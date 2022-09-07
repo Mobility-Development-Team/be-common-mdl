7490009c196ba6fc3bed1c94e8d0441f95a8c4c5
RONALD CHAN HI LONG <ronald.chan.hl@asiaalliedgroup.com>
2022-09-07 16:56:37 +0800

    Add GetBool

--- util/boolutil/boolutil.go
+++ util/boolutil/boolutil.go
@@ -9,3 +9,12 @@ func True() *bool {
 	value := true
 	return &value
 }
+
+// GetBool Returns the bool pointed by a bool pointer b,
+// if b is nil, return false instead
+func GetBool(b *bool) bool {
+	if b != nil {
+		return *b
+	}
+	return false
+}
