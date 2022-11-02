cff79693b42cf3b3b49a45f4cbcb13da62ede23a
RONALD CHAN HI LONG <ronald.chan.hl@asiaalliedgroup.com>
2022-11-02 11:16:00 +0800

    Add GetAutoIncrement

--- util/dbutil/dbutil.go
+++ util/dbutil/dbutil.go
@@ -1,6 +1,8 @@
 package dbutil
 
 import (
+	"errors"
+
 	"gorm.io/gorm"
 )
 
@@ -18,4 +20,23 @@ func PreloadHandler(query *gorm.DB, preloads ...func(*gorm.DB) *gorm.DB) *gorm.D
 		q = q.Scopes(preload)
 	}
 	return q
+}
+
+// GetAutoIncrement gets the auto increment of a table.
+// db must not be nil
+func GetAutoIncrement(db *gorm.DB, tableName string) (int64, error) {
+	if db == nil {
+		return 0, errors.New("db must not be nil")
+	}
+	var ai int64
+	if err := db.
+		Table("INFORMATION_SCHEMA.TABLES").
+		Select("AUTO_INCREMENT").
+		Where("TABLE_SCHEMA = DATABASE()").
+		Where("TABLE_NAME = ?", tableName).
+		Scan(&ai).
+		Error; err != nil {
+		return 0, err
+	}
+	return ai, nil
 }
