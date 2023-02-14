04cffd7748e5e57c4de66f4ce57cf79f618f5aeb
CHUNWO\kester.leung <kester.leung@chunwo.com>
2023-02-14 14:21:07 +0800

    Updated unsafeCase model

---
+++ apis/labour/model.go
@@ -0,0 +1,18 @@
+package labour
+
+import (
+	"github.com/Mobility-Development-Team/be-common-mdl/model"
+	"github.com/Mobility-Development-Team/be-common-mdl/types/intstring"
+)
+
+type (
+	UnsafeCase struct {
+		model.Model
+		CaseNo        string              `json:"caseNo"`
+		CaseStatus    string              `json:"caseStatus"`
+		CaseType      string              `json:"caseType"`
+		ContractRefId intstring.IntString `json:"contractId"`
+		Worker        interface{}         `json:"worker"`
+		Item          interface{}         `json:"item"`
+	}
+)
