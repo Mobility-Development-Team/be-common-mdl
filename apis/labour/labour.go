73b80d8cb42e2726ee96cf8521522392554b3189
CHUNWO\kester.leung <kester.leung@chunwo.com>
2023-02-14 17:11:35 +0800

    Added function to call API - Find ALL unsafe cases for my tasks - cont

---
+++ apis/labour/labour.go
@@ -0,0 +1,40 @@
+package labour
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Mobility-Development-Team/be-common-mdl/apis"
+	"github.com/go-resty/resty/v2"
+)
+
+const (
+	apiLabourMdlUrlBase         = "apis.internal.labour.module.url.base"
+	getAllUnsafeCasesForMyTasks = "%s/unsafe-cases/internal/all"
+)
+
+func GetAllUnsafeCasesForMyTasks(tk string, userRefKey string, criteria UnsafeCaseCriteria) ([]*UnsafeCase, error) {
+	resp := struct {
+		Payload []*UnsafeCase `json:"payload"`
+	}{}
+	client := resty.New()
+	result, err := client.R().SetAuthToken(tk).SetBody(
+		map[string]interface{}{
+			"criteria": criteria,
+			// "opts":     opt,
+			// "preloads": preloadNames,
+		},
+	).Post(
+		fmt.Sprintf(getAllUnsafeCasesForMyTasks, apis.V().GetString(apiLabourMdlUrlBase)),
+	)
+	if err != nil {
+		return nil, err
+	}
+	if !result.IsSuccess() {
+		return nil, fmt.Errorf("machine module returned status code: %d", result.StatusCode())
+	}
+	err = json.Unmarshal(result.Body(), &resp)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Payload, err
+}
