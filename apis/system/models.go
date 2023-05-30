48c49d4b89376d3bfc8ecdfff4804a8a5edf8a00
heiyi <heiyi.pang@asiaallied.group.com>
2023-05-30 17:38:01 +0800

    add the new struct of system to map between party and contract id:

--- apis/system/models.go
+++ apis/system/models.go
@@ -1,6 +1,9 @@
 package system
 
-import "github.com/Mobility-Development-Team/be-common-mdl/types/intstring"
+import (
+	"github.com/Mobility-Development-Team/be-common-mdl/model"
+	"github.com/Mobility-Development-Team/be-common-mdl/types/intstring"
+)
 
 type ContractParty struct {
 	Info struct {
@@ -15,8 +18,18 @@ type ContractParty struct {
 		PartyPrefix   string              `json:"partyPrefix"`
 		SubconRefId   string              `json:"subconRefId"`
 	} `json:"info"`
-	PartyTypeRef string                `json:"partyType"`
-	UserIds      []intstring.IntString `json:"userIds"`
-	GroupIds     []intstring.IntString `json:"groupIds"`
-	RoleIds      []intstring.IntString `json:"roleIds"`
+	PartyTypeRef          string                `json:"partyType"`
+	UserIds               []intstring.IntString `json:"userIds"`
+	GroupIds              []intstring.IntString `json:"groupIds"`
+	RoleIds               []intstring.IntString `json:"roleIds"`
+	ContractPartyMappings []PartyMap            `json:"-" gorm:"foreignKey:PartyId"`
+}
+
+type PartyMap struct {
+	model.Model
+	Status       string              `json:"status"`
+	ContractId   intstring.IntString `json:"contractId" gorm:"index:uk_contract_id_party_id,unique"`
+	PartyId      intstring.IntString `json:"partyId" gorm:"index:uk_contract_id_party_id,unique"`
+	PartyTypeRef string              `json:"partyTypeRef" gorm:"index:uk_contract_id_party_id,unique"`
+	IsPrimary    bool                `json:"isPrimary"`
 }
