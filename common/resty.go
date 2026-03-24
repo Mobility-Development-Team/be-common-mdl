2fcc98845f734bceaac32483fe01871ee168d99a
jasonlaw007 <jason.luo@mattex.com.hk>
2026-03-24 10:04:56 +0800

    [update] improve api try num

--- common/resty.go
+++ common/resty.go
@@ -9,8 +9,8 @@ import (
 func NewResty() *resty.Client {
 	client := resty.New()
 	client.SetRetryCount(3). // 设置最大重试次数为 3 次
-					SetRetryWaitTime(2 * time.Second).    // 设置初始等待时间为 2 秒
-					SetRetryMaxWaitTime(15 * time.Second) // 设置最大等待时间为 15 秒 (指数退避)
+					SetRetryWaitTime(1 * time.Second).    // 设置初始等待时间为 1 秒
+					SetRetryMaxWaitTime(10 * time.Second) // 设置最大等待时间为 10 秒 (指数退避)
 	client.AddRetryCondition(func(r *resty.Response, err error) bool {
 		// 自定义重试条件：如果状态码是 500-599 或 429 (限流)，则重试
 		if r == nil {
